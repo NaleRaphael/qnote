841d2a47a4e462733197a888649c45bda1f49f05
naleraphael <gmccntwxy@gmail.com>
2020-08-04 00:58:25 +0800

    UPDATE: add config for notebook and make config file able to be updated automatically if there is any key missing
    - Some values are added into `NotebookConfig` in order to make us
      implement git-branch-like mechansim. Check out its docstring for
      more details.
    
    - Config file will be checked whenever application is launched,
      and it will be updated automatically there is any key that does
      not exist in the instance `AppConfig`. That is, config file will
      be updated when there are additional keys added in `AppConfig`
      during development (or version update).

--- qnote/config/app.py
+++ qnote/config/app.py
@@ -6,16 +6,52 @@ import json
 __all__ = ['AppConfig']
 
 
+def check_all_keys_exist(template, target):
+    if not isinstance(template, dict):
+        raise TypeError('`template` should be a dict, got %s' % type(template))
+    if not isinstance(target, dict):
+        raise TypeError('`target` should be a dict, got %s' % type(target))
+
+    for k, v in template.items():
+        if k not in target:
+            return False
+        if isinstance(v, dict):
+            if not check_all_keys_exist(v, target[k]):
+                return False
+    return True
+
+
 class Defaults(object):
     pass
 
 
 class ConfigBase(object):
+    """
+    name : str
+        Name of config class.
+    keys : list
+        Keys to control customizable attributes. If an attribute name is
+        not listed in this list, it would be writen into the config file.
+        So that user cannot modify it.
+    """
+    name = ''
+    keys = []
+
     def __init__(self):
         pass
 
     def to_dict(self):
-        raise NotImplementedError
+        return {k: getattr(self, k) for k in self.keys}
+
+    def _check_remaining_kwargs(self, **kwargs):
+        # Check whether there are remaining values for configuration
+        if len(kwargs) != 0:
+            import warnings
+            msg = (
+                f'{self.name}: There are unknown configuration values, '
+                f'did you added them by accident? {kwargs}'
+            )
+            warnings.warn(msg, RuntimeWarning)
 
 
 class AppDefaults(Defaults):
@@ -25,22 +61,25 @@ class AppDefaults(Defaults):
 
 
 class AppConfig(ConfigBase):
+    name = 'app'
+
     def __init__(self, **kwargs):
         super(AppConfig, self).__init__()
         if kwargs is None:
             kwargs = {}     # first initialization
-        self.editor = EditorConfig(**kwargs.pop('editor', {}))
-        self.storage = StorageConfig(**kwargs.pop('storage', {}))
-        self.tag = TagConfig(**kwargs.pop('tag', {}))
-
-        # Check whether there are remaining values for configuration
-        if len(kwargs) != 0:
-            import warnings
-            msg = (
-                f'There are unknown configuration values, did you added them'
-                'by accident? {kwargs}'
-            )
-            warnings.warn(msg, RuntimeWarning)
+        self.editor = EditorConfig(
+            **kwargs.pop(EditorConfig.name, {})
+        )
+        self.storage = StorageConfig(
+            **kwargs.pop(StorageConfig.name, {})
+        )
+        self.tag = TagConfig(
+            **kwargs.pop(TagConfig.name, {})
+        )
+        self.notebook = NotebookConfig(
+            **kwargs.pop(NotebookConfig.name, {})
+        )
+        self._check_remaining_kwargs(**kwargs)
 
     @classmethod
     def load(cls):
@@ -52,7 +91,12 @@ class AppConfig(ConfigBase):
         else:
             with open(AppDefaults.fn_config, 'r') as f:
                 content = json.load(f)
-            return cls(**content)
+
+            # Update config file if there are missing keys
+            config = cls(**content)
+            if not check_all_keys_exist(config.to_dict(), content):
+                config.write()
+            return config
 
     def write(self):
         os.makedirs(AppDefaults.dir_config, exist_ok=True)
@@ -61,9 +105,10 @@ class AppConfig(ConfigBase):
 
     def to_dict(self):
         return {
-            'editor': self.editor.to_dict(),
-            'storage': self.storage.to_dict(),
-            'tag': self.tag.to_dict(),
+            self.editor.name: self.editor.to_dict(),
+            self.storage.name: self.storage.to_dict(),
+            self.tag.name: self.tag.to_dict(),
+            self.notebook.name: self.notebook.to_dict(),
         }
 
 
@@ -73,14 +118,14 @@ class EditorDefaults(Defaults):
 
 
 class EditorConfig(ConfigBase):
+    name = 'editor'
+    keys = ['executable']
+
     def __init__(self, **kwargs):
         super(EditorConfig, self).__init__()
-        self.executable = kwargs.get('executable', EditorDefaults.executable)
-        self.fn_tempfile = kwargs.get('tempfile', EditorDefaults.fn_tempfile)
-
-    def to_dict(self):
-        keys = ['executable']
-        return {k: getattr(self, k) for k in keys}
+        self.executable = kwargs.pop('executable', EditorDefaults.executable)
+        self.fn_tempfile = kwargs.pop('tempfile', EditorDefaults.fn_tempfile)
+        self._check_remaining_kwargs(**kwargs)
 
 
 class StorageDefaults(Defaults):
@@ -90,17 +135,17 @@ class StorageDefaults(Defaults):
 
 
 class StorageConfig(ConfigBase):
+    name = 'storage'
+    keys = ['type', 'dir_root']
+
     def __init__(self, **kwargs):
         super(StorageConfig, self).__init__()
-        self.type = kwargs.get('type', StorageDefaults.type)
+        self.type = kwargs.pop('type', StorageDefaults.type)
         if self.type not in StorageDefaults.valid_types:
             raise ValueError('Type of storage "%s" is not supported, possible'
                 ' choices: %s' % (self.type, StorageDefaults.valid_types))
-        self.dir_root = kwargs.get('dir_root', StorageDefaults.dir_root)
-
-    def to_dict(self):
-        keys = ['type', 'dir_root']
-        return {k: getattr(self, k) for k in keys}
+        self.dir_root = kwargs.pop('dir_root', StorageDefaults.dir_root)
+        self._check_remaining_kwargs(**kwargs)
 
 
 class TagDefaults(Defaults):
@@ -120,13 +165,48 @@ class TagDefaults(Defaults):
 
 
 class TagConfig(ConfigBase):
+    name = 'tag'
+    keys = ['auto_parse', 'auto_remove_from_content']
+
     def __init__(self, **kwargs):
         super(TagConfig, self).__init__()
-        self.auto_parse = kwargs.get('auto_parse', TagDefaults.auto_parse)
-        self.auto_remove_from_content = kwargs.get(
+        self.auto_parse = kwargs.pop('auto_parse', TagDefaults.auto_parse)
+        self.auto_remove_from_content = kwargs.pop(
             'auto_remove_from_content', TagDefaults.auto_remove_from_content
         )
+        self._check_remaining_kwargs(**kwargs)
 
-    def to_dict(self):
-        keys = ['auto_parse', 'auto_remove_from_content']
-        return {k: getattr(self, k) for k in keys}
+
+class NotebookDefaults(Defaults):
+    """
+    fn_head : str
+        A file contains information of current opening notebook. (like the
+        meaing of `HEAD` in git)
+    name_default : str
+        Default name of the notebook that `HEAD` pointing. And it is also the
+        first notebook created when this application is launched for the first
+        time. Therefore, all notes will be saved into this notebook if there
+        is no other notebooks created and opened.
+    name_trash : str
+        Name of the special notebook collecting removed notes.
+    status_n_limit : int
+        Limit of number of notes would be displayed while command
+        `qnote status` is executed.
+    """
+    fn_head = osp.join(AppDefaults.dir_config, 'HEAD')
+    name_default = '[DEFAULT]'
+    name_trash = '[TRASH]'
+    status_n_limit = 5
+
+
+class NotebookConfig(ConfigBase):
+    name = 'notebook'
+    keys = []
+
+    def __init__(self, **kwargs):
+        super(NotebookConfig, self).__init__()
+        self.fn_head = NotebookDefaults.fn_head
+        self.name_default = NotebookDefaults.name_default
+        self.name_trash = NotebookDefaults.name_trash
+        self.status_n_limit = NotebookDefaults.status_n_limit
+        self._check_remaining_kwargs(**kwargs)
